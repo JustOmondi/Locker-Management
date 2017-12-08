f86baae1a73410c611d05afe7f05928afd07d90d
Francois du Toit <francoisdutoit@protonmail.com>
2017-12-08 14:08:14 +0200

    File cleaning and script addition

--- Pi/NodeJS/rsync_pi_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync -vrhe ssh /mnt/sda5/Documents/Absa\ Aliens/Locker-Management/Pi/NodeJS/ pi@24.153.255.117:/home/pi/piServer/
-rsync -vrhe ssh pi@24.153.255.117:/home/pi/piServer/ /mnt/sda5/Documents/Absa\ Aliens/Locker-Management/Pi/NodeJS/
