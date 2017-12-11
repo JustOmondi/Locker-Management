abbba08987a682fd1057033b956e1543c5623247
Omondi Ochieng <omondi.a.ochieng@gmail.com>
2017-12-11 12:51:11 +0200

    Added functionality to obtain selected date from date selector

--- Firebase/public/js/app.js
+++ Firebase/public/js/app.js
@@ -162,12 +162,20 @@ function logsController($scope)
 {
     var logs = $scope;
 
+    var selectedDate;
+
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15, // Creates a dropdown of 15 years to control year,
         today: 'Today',
         clear: 'Clear',
         close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
+        closeOnSelect: false, // Close upon selecting a date,
+        onClose: function()
+        {
+            // console.log(this.get('select', 'yyyy-mm-dd'));
+            selectedDate = this.get('select', 'yyyy-mm-dd');
+        }
     });
+
 }
