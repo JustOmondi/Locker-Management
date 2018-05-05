2333ffa5576cb7eb8833bdbf8313e51881e70f1b
TadMaz <tadi4lyf@yahoo.com>
2018-05-05 16:41:54 +0200

    Touch-ups to Led.py

--- Pi/NodeJS/led.py
+++ Pi/NodeJS/led.py
@@ -3,14 +3,19 @@
 import sys
 import RPi.GPIO as GPIO
 import time
+
+#initialise GPIO PINS
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21,GPIO.OUT)
+
+# Lock or unlock Locker; 
 if sys.argv[1]=="0":
     GPIO.output(21,GPIO.LOW)
 elif sys.argv[1]=="1":
     GPIO.output(21,GPIO.HIGH)
-elif sys.argv[1]=="2":
+elif sys.argv[1]=="2":				#test case, rotates motor to the locks motor , delays the unlocks motor.
     count=0
     while(count<10):
         GPIO.output(21,GPIO.HIGH)
